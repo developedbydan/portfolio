93eccfe58993a34d58adb3b8441c07bb1707fa92
Danijel <developedbydane@gmail.com>
2024-12-05 15:06:19 +0100

    feat(about): Added gsap fade in animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 import ToolsGrid from "../components/ToolsGrid";
 
+import gsap from "gsap";
+
 const About = () => {
+  useEffect(() => {
+    const timeline = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: "sine.inOut",
+      },
+    });
+
+    timeline.to("#toolkit", {
+      opacity: 1,
+      duration: 1,
+    });
+  });
+
   return (
     <Layout>
       <section className="bg-black h-dvh pt-10 px-6 overflow-y-auto">
-        <h2 className="text-secondary text-3xl font-bold text-center">
+        <h2
+          id="toolkit"
+          className="text-secondary text-3xl font-bold text-center opacity-0"
+        >
           My toolkit
         </h2>
         <ToolsGrid />
